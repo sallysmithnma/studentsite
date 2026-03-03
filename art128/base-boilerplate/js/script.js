9b271f6e8df191a74b8c9e8f28c9ee531b18dd31
KCC New Media Arts <gargiulo@hawaii.edu>
2026-03-02 16:28:33 -1000

    updated art 128 projects

--- art128/base-boilerplate/js/script.js
+++ art128/base-boilerplate/js/script.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     //JAVASCRIPT FUNCTIONS GO HERE
-    console.log('Javascript is ready');
 
-
-});
+})
--- art128/base-site-subpages/js/script.js
+++ art128/base-site-subpages/js/script.js
@@ -1,16 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-
-    //TOGGLE NAV MOBILE MENU FOR SMALL SCREENS
+document.addEventListener("DOMContentLoaded", function() {
+  
+    //TOGGLE MENU NAV VIA ADDING/REMOVING A DATA-ATTRIBUTE
     const menubutton = document.querySelector('.menu-button');
-    const menunav = document.querySelector('.toggle-nav');
-    menubutton.addEventListener('click', function () {
-        if (menunav.getAttribute('data-navstate') === 'open') {
-            // if true do this
-            menunav.setAttribute('data-navstate', 'closed');
-        } else {
-            // else (if false) do this:
-            menunav.setAttribute('data-navstate', 'open');
-        };
+    const menunav = document.querySelector('.toggle-nav'); 
+    menubutton.addEventListener('click', function() {
+      if (menunav.getAttribute('data-navstate') === 'open') {
+          menunav.setAttribute('data-navstate', 'closed');
+      } else {
+          menunav.setAttribute('data-navstate', 'open');
+      }
     });
-
-});
+  
+  });
+  
